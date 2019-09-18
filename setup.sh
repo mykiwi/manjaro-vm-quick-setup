5830478a332fb79567f3413c7ede742beb4761b7
Romain Gautier <mykiwi@users.noreply.github.com>
2019-09-19 00:19:47 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,3 +4,30 @@ sudo pacman -Syyu
 sudo pacman -S gcc make linux$(uname -r|sed 's/\W//g'|cut -c1-2)-headers virtualbox-guest-iso
 sudo mount -o loop /usr/lib/virtualbox/additions/VBoxGuestAdditions.iso /mnt
 sudo /mnt/VBoxLinuxAdditions.run
+
+cat <<'EOF' > /etc/pamac.conf
+RemoveUnrequiredDeps
+RefreshPeriod = 6
+NoUpdateHideIcon
+EnableAUR
+CheckAURUpdates
+BuildDirectory = /var/tmp
+KeepNumPackages = 3
+OnlyRmUninstalled
+DownloadUpdates
+MaxParallelDownloads = 4
+EOF
+
+pamac install --no-confirm \
+  docker \
+  docker-compose \
+  docker-machine \
+  firefox-developer-edition \
+  neovim \
+  pigz # for docker
+
+pamac build --no-confirm \
+  brave-bin \
+  jetbrains-toolbox \
+  sublime-text-dev \
+  vscodium-bin
