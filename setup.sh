22ccff9879197a964aff544459b8d5dce7a88767
Romain Gautier <mail@romain.sh>
2020-02-19 18:41:00 +0100

    add some php and private stuff

--- setup.sh
+++ setup.sh
@@ -1,10 +1,24 @@
 #!/bin/bash
 set -xe
 
+while [ $# -gt 0 ]
+do
+    case $1 in
+    --vbox)
+        VIRTUALBOX=1
+        ;;
+    --secrets)
+        SECRETS=1
+        ;;
+
+    esac
+    shift
+done
+
 sudo pacman -Syyu
 sudo pacman -Syyu
 
-if [ "$1" === "vbox" ]; then
+if [ -n "${VIRTUALBOX}" ]; then
     sudo pacman -S gcc make linux$(uname -r|sed 's/\W//g'|cut -c1-2)-headers virtualbox-guest-iso
     sudo mount -o loop /usr/lib/virtualbox/additions/VBoxGuestAdditions.iso /mnt
     sudo /mnt/VBoxLinuxAdditions.run
@@ -30,7 +44,9 @@ pamac install --no-confirm \
   docker-machine \
   firefox-developer-edition \
   neovim \
-  pigz # for docker
+  pigz \
+  zsh
+# pigz is for docker
 
 pamac build --no-confirm \
   brave-bin \
@@ -42,3 +58,42 @@ pamac build --no-confirm \
 sudo groupadd docker || true
 sudo usermod -aG docker $USER
 sudo systemctl enable docker
+
+pamac install --no-confirm \
+  nodejs \
+  npm \
+  php \
+  php-apcu \
+  php-gd \
+  php-intl \
+  php-mongodb \
+  php-pgsql \
+  php-redis \
+  php-sqlite \
+  php-sodium \
+  xdebug
+pamac build --no-confirm \
+  php-amqp \
+  php-blackfire
+
+sudo curl -sSL -o /usr/local/bin/composer https://getcomposer.org/composer-stable.phar
+sudo chmod +x /usr/local/bin/composer
+wget https://get.symfony.com/cli/installer -O - | bash
+sudo ln -s /home/mykiwi/.symfony/bin/symfony /usr/local/bin/symfony
+
+
+# Private stuff
+if [ -n "${SECRETS}" ]; then
+    keybase login --devicename $(date "+%Y-%m")-vm
+    keybase fs cp -r /keybase/private/mykiwi/setup/linux/vm-ssh-key ~/.ssh
+    chmod 700 ~/.ssh
+    chmod 600 ~/.ssh/id*
+    chmod 655 ~/.ssh/id*.pub
+
+    mkdir -p ~/dev/github.com/mykiwi
+    git clone --recursive git@github.com:mykiwi/dotfiles.git ~/dev/github.com/mykiwi/dotfiles
+    sh -c 'cd ~/dev/github.com/mykiwi/dotfiles && install.sh'
+    git clone --recursive git@github.com:mykiwi/dotfiles.private.git ~/dev/github.com/mykiwi/dotfiles.private
+    sh -c 'cd ~/dev/github.com/mykiwi/dotfiles.private/ && install.sh'
+    chsh -s /usr/bin/zsh
+fi
